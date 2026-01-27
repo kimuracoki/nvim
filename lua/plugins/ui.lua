a533dad607a38ef2676bab40e7a012c069c0cd2f
kimura.coki <kimura.coki@gmail.com>
2026-01-27 10:17:03 +0900

    Add notification

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -269,23 +269,23 @@ return {
     end,
   },
 
-  -- 通知システムの改善（一時的に無効化）
-  -- {
-  --   "rcarriga/nvim-notify",
-  --   config = function()
-  --     require("notify").setup({
-  --       stages = "fade_in_slide_out",
-  --       timeout = 3000,
-  --       max_height = function()
-  --         return math.floor(vim.o.lines * 0.75)
-  --       end,
-  --       max_width = function()
-  --         return math.floor(vim.o.columns * 0.75)
-  --       end,
-  --     })
-  --     vim.notify = require("notify")
-  --   end,
-  -- },
+  -- 通知システムの改善
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        stages = "fade_in_slide_out",
+        timeout = 3000,
+        max_height = function()
+          return math.floor(vim.o.lines * 0.75)
+        end,
+        max_width = function()
+          return math.floor(vim.o.columns * 0.75)
+        end,
+      })
+      vim.notify = require("notify")
+    end,
+  },
 
   -- ミニマップ（VSCodeの右側コードマップ）
   {
