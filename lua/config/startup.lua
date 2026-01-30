ab422bbb3dde911125c93a719e7ebef9794b4166
kimura.coki <kimura.coki@gmail.com>
2026-01-30 17:33:37 +0900

    Move noname

--- lua/config/startup.lua
+++ lua/config/startup.lua
@@ -7,7 +7,7 @@ function M.setup_layout()
       vim.cmd("Neotree show")
       vim.cmd("wincmd l") -- エディタに移動
     end
-    
+
     -- 問題パネルを開く（Troubleの設定で下部に表示される）
     vim.defer_fn(function()
       vim.cmd("Trouble diagnostics")
