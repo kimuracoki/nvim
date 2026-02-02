e2a2807c732cbff8267ea44afcb1215d34a285c8
kimura.coki <kimura.coki@gmail.com>
2026-02-02 11:29:25 +0900

    Upd default color

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "plugins" }, -- lua/plugins/*.lua を全部読む
   },
   install = {
-    colorscheme = { "tokyonight" }, -- デフォルトカラースキーム
+    colorscheme = { "catppuccin" }, -- デフォルトカラースキーム
   },
   checker = {
     enabled = false, -- 自動アップデートチェックはとりあえずOFF
